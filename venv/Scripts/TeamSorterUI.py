10e90b813c8f8c91611edda146b7e3e3a7705d45
SJiet <jiet9000@gmail.com>
2020-12-30 19:11:16 +0800

    TeamSorterUI file

---
+++ venv/Scripts/TeamSorterUI.py
@@ -0,0 +1,35 @@
+
+
+# CSVReader.py
+
+path = ""
+
+csvColumnIndex = ""
+
+
+
+# SkillDefiner.csv
+
+# Skill 1 - Experience:
+experienceLevel1 = "1"
+experienceLevel2 = "2"
+experienceLevel3 = "3"
+experienceLevel4 = "4"
+experienceLevel5 = "5"
+
+# Skill 2 - Throws
+throwingLevel1 = "I can barely hold a disc"
+throwingLevel2 = "I can only do short distance backhand"
+throwingLevel3 = "I\'m able to do both backhand and forehand throws"
+throwingLevel4 = "I can huck"
+throwingLevel5 = "I can huck, hammer, break. Call me a solid handler"
+
+# Skill 3 - Speed
+speedLevel1 = "I can huck, hammer, break. Call me a solid handler"
+speedLevel2 = "I can huck"
+speedLevel3 = "I\'m able to do both backhand and forehand throws"
+speedLevel4 = "I can only do short distance backhand"
+speedLevel5 = "I can barely hold a disc"
+
+
+# TeamSorter.py
